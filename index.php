e39796b493ffd4c85e7569cfb2aeb20b6de4a5d7
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2023-10-08 16:18:29 -0700

    numbers

--- index.php
+++ index.php
@@ -7,16 +7,46 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="get">
-        <label>username:</label><br>
-        <input type="text" name="username"><br>
-        <label>password:</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log in" >
-    </form>
+   <form action="index.php" method="post">
+       <label>x:</label>
+       <input type="text" name="x"><br>
+       <label>y:</label>
+       <input type="text" name="y"><br>
+       <label>z:</label>
+       <input type="text" name="z"><br>
+       <input type="submit" value="total">
+   </form>        
+
+<!-- <form action="index.php" method="post">
+       <label>quantity: </label><br>
+       <input type="text" name="quantity"><br>
+       <input type="submit" value="total">
+    </form> -->
 </body>
 </html>
 
 <?php 
-    echo $GET["username"];
+
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+    $z = $_POST["z"];
+    $total = null;
+
+    // $total = abs($x); is the absolute value
+    // $total = round($x); is rounded to nearest whole
+    // $total = pow($x, $y); total is x to the power of y
+    // $total = sqrt($x); is the square root of a number.
+    // $total = max($x, $y, $z); highest number enter.
+
+    echo $total;
+    // $item = "pizza";
+    // $price = 5.99;
+    // $quantity = $_POST["quantity"];
+    // $total = null;
+
+    // $total = $quantity * $price;
+
+    //  echo"You have ordered {$quantity} x {$item}/s <br>";
+    //  echo"Your total is: \${$total}";
+   
 ?>
